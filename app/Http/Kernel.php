e6eafe22fc18f804d03fd1c9600c0d3dd6c4fd53
Masud99Rana <Masudrana917775@gmail.com>
2019-07-01 07:43:37 +0600

    middleware added for all routes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,6 +61,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'verifyCategoriesCount' => VerifyCategoriesCount::class,
         'admin' => \App\Http\Middleware\VerifyIsAdmin::class,
     ];
 
